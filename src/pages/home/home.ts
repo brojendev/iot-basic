20c3572ed41bc85c3c6186496695d6ea53e6cf72
brojendev <brojen.dev@gmail.com>
2018-05-16 11:34:05 +0530

    Init project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Hotspot, HotspotNetwork } from '@ionic-native/hotspot';
 
+declare let WifiWizard: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  networks: any = [];
+
+  constructor(
+    public navCtrl: NavController,
+    private hotspot: Hotspot,
+    private platform: Platform
+  ) {
+    this.startScan();
+  }
+
+  startScan(){
+    this.platform.ready().then(() => {
+      let self = this;
+      this.hotspot.scanWifi().then((networks: Array<HotspotNetwork>) => {
+        console.log('networks', networks);
+        self.networks = networks;
+      });
+    });
+
 
   }
 
